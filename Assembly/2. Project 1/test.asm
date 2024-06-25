084ac018f39f8310c01ae6cf27047b4ded238b35
PLM-18 <philemonmuleya@gmail.com>
2024-06-25 22:40:22 +0200

    Changes 2

--- Assembly/2. Project 1/test.asm
+++ Assembly/2. Project 1/test.asm
@@ -1,18 +1,5 @@
-section .data
-    hello db 'Hello, World!', 0
-
-section .text
-    global _start
-
+.global _start
 _start:
-    ; write the string to stdout
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, hello
-    mov edx, 13
-    int 0x80
-
-    ; exit the program
-    mov eax, 1
-    xor ebx, ebx
-    int 0x80
+    mov r0, #30
+    mov r7, #1
+    swi 0
---
+++ ReadMe.md
@@ -0,0 +1,2 @@
+#   This repository is used to lean new Languages
+#   There is no particular structure to this repository 
