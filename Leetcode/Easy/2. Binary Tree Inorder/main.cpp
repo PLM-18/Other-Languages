56245934622daeb3af9063a6a9c2d9a287c7ea53
PLM-18 <philemonmuleya@gmail.com>
2024-09-13 14:46:48 +0200

    Added Binary Tree Inorder Print

---
+++ Leetcode/Easy/2. Binary Tree Inorder/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        stack<TreeNode*> s;
+        TreeNode* cur = root;
+        while (cur != nullptr || !s.empty()) {
+            while (cur != nullptr) {
+                s.push(cur);
+                cur = cur->left;
+            }
+            cur = s.top();
+            s.pop();
+            res.push_back(cur->val);
+            cur = cur->right;
+        }
+        return res;
+    }
+};
+
+int main(){
+    Solution s;
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->right->left = new TreeNode(0);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+    root->left->right->left = new TreeNode(6);
+    root->left->right->right = new TreeNode(7);
+    root->right->right = new TreeNode(8);
+    root->right->right->right = new TreeNode(0);
+    root->right->right->left = new TreeNode(9);
+    vector<int> res = s.inorderTraversal(root);
+    int size = (int)res.size();
+    for (int i = 0; i < size; i++) {
+        cout << res[i] << " ";
+    }
+    cout << endl;
+}
